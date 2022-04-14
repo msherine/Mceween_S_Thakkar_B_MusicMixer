35714c49e82fe5ffba92f503e8012f390b1ae2d0
msherine <90708400+msherine@users.noreply.github.com>
2022-04-13 23:58:53 -0400

    js fixes

--- js/main.js
+++ js/main.js
@@ -1,4 +1,23 @@
 (() => {
 
+const instruments = document.querySelectorAll(".mixer_instruments img"),
+  dropArea = document.querySelectorAll(".inst_drop"),
+  instrumentArea = document.querySelectorAll(".mixer_instruments"),
+
+function grabbed(event){
+  console.log("Your chosen instrument");
+  event.dataTransfer.setData("idSaved",this.id);
+  event.dataTransfer.setData("chosenInstrument", this.dataset.sound);
+}
+
+function dragged(event){
+  event.preventDefault();
+  console.log("instrument dragged")
+}
+
+function selected(event){
+  event.preventDefault();
+  
+}
 
 })();
